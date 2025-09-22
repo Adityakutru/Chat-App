5076b3c3e2435bf91ec76003cc7b9cd8005e58cb
Aditya Mohapatra <adityamohapatra24@gmail.com>
2025-09-22 10:28:00 +0530

    Setup message model

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import authRoutes from './routes/auth.route.js'
+import messageRoutes from './routes/message.route.js'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
 import { connectDB } from './lib/db.js'
@@ -15,6 +16,7 @@ app.use(cookieParser())
 
 const PORT = process.env.PORT || 4001
 app.use("/api/auth", authRoutes)
+app.use("/api/message", messageRoutes)
 
 app.listen(PORT,()=>{
     console.log("Server running on port: 5001")
